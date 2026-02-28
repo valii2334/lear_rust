b76f9b70ad838f1719f34241e3c06f946418e483
Valentin Lazar <valentin.lazar.personal.email@gmail.com>
2026-02-28 13:59:09 +0200

    Chapter 2 - exercise 7 - passing values vs references

---
+++ chapter2/exercise7.rs
@@ -0,0 +1,9 @@
+fn print_country(country_name: &String) {
+  println!("{}", *country_name);
+}
+
+fn main() {
+  let country = String::from("Austria");
+  print_country(&country);
+  print_country(&country);
+}
